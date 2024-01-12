8bdf521b0ac2b0d6e414e954ef3be9068f02b533
Lait Igor <laitigorofficial@gmail.com>
2024-01-12 20:43:34 +0100

    create initial routes for sign in, create sign up form, install shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-
+import { Routes, Route } from 'react-router-dom';
+import SignInForm from './Auth/forms/SignInForm';
+import SignUpForm from './Auth/forms/SignUpForm';
+import AuthLayout from './Auth/AuthLayout';
+import RootLayout from './Root/RootLayout';
+import { Home } from './Root/pages';
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">
-      Hello Pirstgram
-    </div>
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout/>}>
+          <Route path='/sign-in' element={<SignInForm />} />
+          <Route path='/sign-up' element={<SignUpForm />} />
+        </Route>
+        {/* Private Routes */}
+        <Route element={<RootLayout/>}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
---
+++ src/Auth/forms/SignUpForm.tsx
@@ -0,0 +1,116 @@
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Link } from "react-router-dom"
+import { useForm } from "react-hook-form"
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { SignupValidation } from "@/lib/validation"
+import Loader from "../../components/shared/Loader";
+
+
+
+const SignUpForm = () => {
+  const isLoading = false;
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: '',
+      username: "",
+      email: '',
+      password: ''
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    // const newUser = await createUserAccount(values);
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="./assets/images/logo.svg" alt="Logo" />
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">To use Pristgram, please enter your details</p>
+        
+        <form className='flex-col gap-5 w-full mt-4' onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type='text' className='shad-input' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input type='text' className='shad-input' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type='email' className='shad-input' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type='password' className='shad-input' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary w-full mt-4">
+            {isLoading ? (
+              <div className='flex-center gap-2'><Loader/>Loading...</div>
+            ) : 'Sign up'}
+          </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account?
+            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+          </p>
+        </form>
+      </div>
+  </Form>
+  )
+}
+
+export default SignUpForm
